39a4cf73f543e826e9a351af576cd86aa9b92c64
Boris Stukov <stukov.boris@gmail.com>
2017-04-03 17:18:26 +0600

    Create class Game, create card_desk

--- game.rb
+++ game.rb
@@ -1,1 +1,20 @@
-test
+class Game
+
+  attr_accessor :card_deck
+
+  def initialize
+    @card_deck = []
+    @cards = [(2..10).to_a, 'J', 'Q', 'K', 'A'].flatten
+    @suits = ["\u2661", "\u2662", "\u2667", "\u2664"]
+
+    @cards.each do |card|
+      @suits.each do |suit|
+        @card_deck << "#{card}" + "#{suit}"
+      end
+    end
+  end
+end
+
+game = Game.new
+
+puts game.card_deck
